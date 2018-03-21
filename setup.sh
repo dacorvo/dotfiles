d025806808f01ee0673d1bcd05f22e33a48491b7
David Corvoysier <david.corvoysier@gmail.com>
2018-03-21 11:35:16 +0100

    added tmux conf to setup

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ cp -r bin ~
 cat .bashrc >> ~/.bashrc
 cp .dircolors ~
 cp .git* ~
+cp .tmux.conf ~
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~
 vim +PluginInstall +qall
